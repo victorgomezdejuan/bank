10d32eee3e7ea9c40c840030f117d49fa0736106
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-10-04 09:56:07 +0200

    We can add a withdrawal entry to an empty account

---
+++ Bank/Withdrawal.cs
@@ -0,0 +1,24 @@
+namespace Bank;
+
+public class Withdrawal : IAccountEntry
+{
+    public Withdrawal(DateOnly date, int amount)
+    {
+        Amount = amount;
+        Date = date;
+        
+    }
+
+    public DateOnly Date { get; }
+    public int Amount { get; }
+
+    public int CalculateAmountWithSign() => -Amount;
+
+    public override string ToString() => $"{Date:yyyy-MM-dd} | | {Amount}";
+
+    public override bool Equals(object? obj) => obj is Withdrawal other && Equals(other);
+
+    public bool Equals(Withdrawal other) => Date.Equals(other.Date) && Amount == other.Amount;
+
+    public override int GetHashCode() => HashCode.Combine(Date, Amount);
+}
