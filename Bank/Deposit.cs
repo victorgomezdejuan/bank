38e29d0baac007fa625babbe620c01dd2b89c717
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-10-04 09:56:33 +0200

    Add Deposit's methods for equality comparison

--- Bank/Deposit.cs
+++ Bank/Deposit.cs
@@ -13,8 +13,15 @@ public class Deposit : IAccountEntry
     }
 
     public DateOnly Date { get; }
-
     public int Amount { get; }
 
     public int CalculateAmountWithSign() => Amount;
+
+    public override string ToString() => $"{Date:yyyy-MM-dd} | {Amount} |";
+
+    public override bool Equals(object? obj) => obj is Deposit other && Equals(other);
+
+    public bool Equals(Deposit other) => Date.Equals(other.Date) && Amount == other.Amount;
+
+    public override int GetHashCode() => HashCode.Combine(Date, Amount);
 }
