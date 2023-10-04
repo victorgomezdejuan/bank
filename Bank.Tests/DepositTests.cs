53de41be4a46f42f8afb2418b6a681e61404d454
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-10-04 09:41:03 +0200

    A deposit entry with negative amout cannot be created

--- Bank.Tests/DepositTests.cs
+++ Bank.Tests/DepositTests.cs
@@ -3,9 +3,17 @@ namespace Bank.Tests;
 public class DepositTests
 {
     [Fact]
-    public void AmountZeroIsNotValid()
+    public void ZeroAmountIsNotValid()
     {
         static void action() => new Deposit(new DateOnly(2021, 1, 1), 0);
+
+        Assert.Throws<ArgumentException>(action);
+    }
+
+    [Fact]
+    public void NegativeAmountIsNotValid()
+    {
+        static void action() => new Deposit(new DateOnly(2021, 1, 1), -100);
 
         Assert.Throws<ArgumentException>(action);
     }
