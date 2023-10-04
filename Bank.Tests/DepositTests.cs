96e52edf527babc6af764e63f7635013362abcf3
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-10-04 15:13:52 +0200

    Check deposit and withdrawal creation happy paths and amount with sign calculation

--- Bank.Tests/DepositTests.cs
+++ Bank.Tests/DepositTests.cs
@@ -17,4 +17,22 @@ public class DepositTests
 
         Assert.Throws<ArgumentException>(action);
     }
+
+    [Fact]
+    public void PositiveAmountIsValid()
+    {
+        var deposit = new Deposit(new DateOnly(2021, 1, 1), 100);
+        
+        Assert.Equal(new DateOnly(2021, 1, 1), deposit.Date);
+        Assert.Equal(100, deposit.Amount);
+    }
+
+    [Fact]
+    public void CalculateAmountWithSignReturnsPositiveAmount()
+    {
+        var irrelevantDate = new DateOnly(2021, 1, 1);
+        var deposit = new Deposit(irrelevantDate, 100);
+        
+        Assert.Equal(100, deposit.CalculateAmountWithSign());
+    }
 }
--- Bank.Tests/WithdrawalTests.cs
+++ Bank.Tests/WithdrawalTests.cs
@@ -17,4 +17,22 @@ public class WithdrawalTests
 
         Assert.Throws<ArgumentException>(action);
     }
+
+    [Fact]
+    public void PositiveAmountIsValid()
+    {
+        var withdrawal = new Withdrawal(new DateOnly(2021, 1, 1), 100);
+        
+        Assert.Equal(new DateOnly(2021, 1, 1), withdrawal.Date);
+        Assert.Equal(100, withdrawal.Amount);
+    }
+
+    [Fact]
+    public void CalculateAmountWithSignReturnsNegativeAmount()
+    {
+        var irrelevantDate = new DateOnly(2021, 1, 1);
+        var withdrawal = new Withdrawal(irrelevantDate, 100);
+        
+        Assert.Equal(-100, withdrawal.CalculateAmountWithSign());
+    }
 }
