7ad570633341c81a3b097b3c7c707a3c61faf97a
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-10-04 10:36:55 +0200

    Confirmed that more than one withdrawal entry can be added to an account

--- Bank.Tests/AddWithdrawalTests.cs
+++ Bank.Tests/AddWithdrawalTests.cs
@@ -1,12 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Bank.Tests;
 
-namespace Bank.Tests
+public class AddWithdrawalTests
 {
-    public class AddWithdrawalTests
+    [Fact]
+    public void WithdrawalAsFirstEntry()
     {
-        
+        List<IAccountEntry> entries = new();
+        var account = new Account(entries);
+        var withdrawal = new Withdrawal(new DateOnly(2021, 1, 1), 100);
+
+        account.AddEntry(withdrawal);
+
+        Assert.Single(entries);
+        Assert.Equal(withdrawal, entries[0]);
+    }
+
+    [Fact]
+    public void WithdrawalAsSecondEntry()
+    {
+        List<IAccountEntry> entries = new();
+        var account = new Account(entries);
+        var withdrawal = new Withdrawal(new DateOnly(2021, 1, 1), 100);
+        var withdrawal2 = new Withdrawal(new DateOnly(2021, 1, 2), 200);
+
+        account.AddEntry(withdrawal);
+        account.AddEntry(withdrawal2);
+
+        Assert.Equal(2, entries.Count);
+        Assert.Equal(withdrawal2, entries[1]);
     }
 }
