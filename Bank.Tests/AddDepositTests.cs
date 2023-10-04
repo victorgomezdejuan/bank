2e8e3d917c4b4bd79d77073bc80086ce9a63afde
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-10-04 09:39:32 +0200

    We can add a second deposit entry to an account

--- Bank.Tests/AddDepositTests.cs
+++ Bank.Tests/AddDepositTests.cs
@@ -7,12 +7,26 @@ public class AddDepositTests
     {
         List<IAccountEntry> entries = new();
         var account = new Account(entries);
-        var date = new DateOnly(2021, 1, 1);
-        var deposit = new Deposit(date, 100);
+        var deposit = new Deposit(new DateOnly(2021, 1, 1), 100);
 
         account.AddEntry(deposit);
 
         Assert.Single(entries);
         Assert.Equal(deposit, entries[0]);
+    }
+
+    [Fact]
+    public void DepositAsSecondEntry()
+    {
+        List<IAccountEntry> entries = new();
+        var account = new Account(entries);
+        var deposit = new Deposit(new DateOnly(2021, 1, 1), 100);
+        var deposit2 = new Deposit(new DateOnly(2021, 1, 2), 200);
+
+        account.AddEntry(deposit);
+        account.AddEntry(deposit2);
+
+        Assert.Equal(2, entries.Count);
+        Assert.Equal(deposit2, entries[1]);
     }
 }
